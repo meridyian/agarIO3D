633a893bda264b9a59c3064fff074c15a5a6b1af
meridyian <74350878+meridyian@users.noreply.github.com>
2023-08-13 01:08:32 +0300

    new input system installed

--- Assets/PlayerDataNetworked.cs
+++ Assets/PlayerDataNetworked.cs
@@ -1,18 +1,48 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Fusion;
+using UnityEngine.UI;
+using Behaviour = UnityEngine.Behaviour;
 
-public class PlayerDataNetworked : MonoBehaviour
+public class PlayerDataNetworked : NetworkBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [Networked(OnChanged = nameof(UsernameChanged))]
+
+    public string UserName { get; set; }
+
+    public Text _playernameEntryText;
+    public static PlayerDataNetworked NetworkedDataInstance;
+
+    private void Awake()
     {
-        
+        if (NetworkedDataInstance == null)
+        {
+            NetworkedDataInstance = this;
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    public override void Spawned()
     {
-        
+        _playernameEntryText.text = UserName;
+        if (Object.HasStateAuthority)
+        {
+            var userName = FindObjectOfType<PlayerData>().GetUserName();
+            SendNameRpc(userName);
+            // küçük u ile de denesene
+            _playernameEntryText.text = UserName;
+        }
+    }
+
+    private static void UsernameChanged(Changed<PlayerDataNetworked> changed)
+    {
+        changed.Behaviour.UserName = changed.Behaviour._playernameEntryText.text;
+    }
+
+    [Rpc(RpcSources.All, RpcTargets.StateAuthority)]
+    public void SendNameRpc(string name)
+    {
+        UserName = name;
     }
 }
